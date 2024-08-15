66b95cdc8adea6a5549cb270cc4a3ddda972f99a
Tanzeel Alam <tanzeelalam@gmail.com>
2024-08-15 12:18:59 +0530

    Add dog.go and go.mod

---
+++ dog.go
@@ -0,0 +1,5 @@
+import "strings"
+
+func WhenGrownUp(s string) string {
+	return "When the puppy grows up it says.. " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tanzeelalam/dog
+
+go 1.23.0
